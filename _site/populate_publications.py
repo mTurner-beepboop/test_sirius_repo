abdad60e863bccf076e2f0df3ffde1a7b353b840
Mark Turner <2386300T@student.gla.ac.uk>
2022-06-21 15:56:22 +0100

    addind files

--- _site/populate_publications.py
+++ _site/populate_publications.py
@@ -5,15 +5,14 @@ URL = "https://serpapi.com/search.json?engine=google_scholar_author"
 
 scholar_id = "0OupgU0AAAAJ" #MKhamis Author ID
 api_key = "90241054b897df69d69b36eff0e50d29064f3e85f5784131f02fcf0e81ab484f" #SerpAPI private key
+num_results = "100"
+sort = "pubdate" #Get most recent publications
 
-query_URL = URL + "&author_id=" + scholar_id + "&api_key=" + api_key
+query_URL = URL + "&author_id=" + scholar_id + "&api_key=" + api_key + "&num=" + num_results + "&sort=" + sort
 
 r = requests.get(url = query_URL)
 
 data = r.json()
-
-print(data['author'])
-print(data['articles'])
 
 out_file = open("temp.json", "w")
 
